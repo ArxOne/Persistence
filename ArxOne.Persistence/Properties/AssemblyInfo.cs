12f398f44436d7e1fafd62be64982edc9b16602d
Pascal Craponne <pascal@craponne.fr>
2015-08-01 22:47:11 +0200

    Project setup

---
+++ ArxOne.Persistence/Properties/AssemblyInfo.cs
@@ -0,0 +1,25 @@
+﻿#region Arx One FTP
+// Arx One FTP
+// A simple FTP client
+// https://github.com/ArxOne/FTP
+// Released under MIT license http://opensource.org/licenses/MIT
+#endregion
+
+using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle("ArxOne Persistence")]
+
+[assembly: AssemblyDescription("A simple persistence library. Simply mark properties with attributes and they are persisted in registry.")]
+[assembly: AssemblyCompany("Arx One")]
+[assembly: AssemblyProduct("ArxOne.Persistence")]
+[assembly: AssemblyCopyright("MIT license http://opensource.org/licenses/mit-license.php")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en")]
+
+[assembly: AssemblyVersion("0.1")]
+
+[assembly: CLSCompliant(true)]
