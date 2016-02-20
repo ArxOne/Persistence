e1e6638f6eeddb0e01ab8887dd5e2f511338d88f
Pascal Craponne <pascal@craponne.fr>
2016-02-21 00:15:13 +0100

    Added test project

---
+++ TestApplication/Program.cs
@@ -0,0 +1,26 @@
+﻿#region Arx One Persistence
+// Arx One Persistence
+// The one who keeps you alive after death
+// https://github.com/ArxOne/Persistence
+// MIT License
+#endregion
+
+using ArxOne.Persistence.Serializer;
+
+[assembly: RegistryPersistence("ArxOne.Persistence.Test")]
+
+namespace TestApplication
+{
+    using ArxOne.Persistence;
+
+    public static class Program
+    {
+        [Persistent("IntValue", DefaultValue = 0)]
+        public static int IntValue { get; set; }
+
+        public static void Main(string[] args)
+        {
+            ++IntValue;
+        }
+    }
+}
