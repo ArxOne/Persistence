085117e7a3f9f5250e2e939f9f6c80b1ae5c39ae
Pascal Craponne <pascal@craponne.fr>
2016-05-18 11:38:03 +0200

    Upgraded packages

--- ArxOne.Persistence/Properties/AssemblyInfo.cs
+++ ArxOne.Persistence/Properties/AssemblyInfo.cs
@@ -19,6 +19,6 @@ using System.Resources;
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
 
-[assembly: AssemblyVersion("0.3.1")]
+[assembly: AssemblyVersion("0.3.2")]
 
 [assembly: CLSCompliant(true)]
